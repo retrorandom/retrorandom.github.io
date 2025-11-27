49aac736c99a32fab88c67526281dea8b05594dc
Lilly <70628884+retrorandom@users.noreply.github.com>
2025-11-27 20:33:58 +0800

    new new new new site

--- README.md
+++
@@ -1,3 +0,0 @@
-# retrorandom.github.io
-
-this is for the website please click here to go: https://www.retrorandom.lol/
--- assets/scripts/lightbox.js
+++
@@ -1,8 +0,0 @@
-function openLightbox(img) {
-    document.getElementById("lightbox-img").src = img.src;
-    document.getElementById("lightbox").style.display = "flex";
-}
-
-function closeLightbox() {
-    document.getElementById("lightbox").style.display = "none";
-}
